f6fe147c759a9302c6f1bbaa375e846d157c9dc4
soohyeok <soohyeok0822@gmail.com>
2021-01-11 21:25:20 -0500

    pug

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+app.set("views engine", "pug");
+
 const handleListening = () => console.log(`Server started at ${PORT}`);
 
 app.listen(PORT, handleListening);
